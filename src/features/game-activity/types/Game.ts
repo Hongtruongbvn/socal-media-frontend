11d2723e0a5a678002ca9fa79a366dfab81df3a9
NICTruongbcn <hongtruongbvn2k5@gmail.com>
2026-05-16 19:26:43 +0700

    Revert "khoi tao du an"
    This reverts commit 0ddbfe43be79c9aaf37c975fad2ed35014ee62d9.

--- src/features/game-activity/types/Game.ts
+++
@@ -1,8 +0,0 @@
-// Định nghĩa cấu trúc dữ liệu cho một game từ kết quả tìm kiếm IGDB
-export interface GameSearchResult {
-  id: number;
-  name: string;
-  cover?: {
-    image_id: string;
-  };
-}
